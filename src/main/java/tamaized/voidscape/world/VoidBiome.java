2025f705727b7ccf494e0c86c906d04fc715e7fe
Tamaized <9671313+Tamaized@users.noreply.github.com>
2020-08-01 09:44:01 -0400

    sanify the chunk gen
    Signed-off-by: Tamaized <9671313+Tamaized@users.noreply.github.com>

--- src/main/java/tamaized/voidscape/world/VoidBiome.java
+++ src/main/java/tamaized/voidscape/world/VoidBiome.java
@@ -3,6 +3,8 @@ package tamaized.voidscape.world;
 import com.google.common.collect.ImmutableList;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.audio.BackgroundMusicSelector;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeAmbience;
 import net.minecraft.world.biome.MoodSoundAmbience;
@@ -24,8 +26,8 @@ public class VoidBiome extends Biome {
 						surfaceBuilder(SurfaceBuilder.DEFAULT, SURFACE_BUILDER_CONFIG).
 						precipitation(Biome.RainType.NONE).
 						category(Category.NONE).
-						depth(-2.0F).
-						scale(0.2F).
+						depth(-3.0F).
+						scale(1.8F).
 						temperature(0.0F).
 						downfall(0.0F).
 						parent(null).
@@ -35,6 +37,7 @@ public class VoidBiome extends Biome {
 								func_235248_c_(0x000000). // waterFogColor
 								func_235239_a_(0x000000). // fog color
 								func_235243_a_(MoodSoundAmbience.field_235027_b_).
+								func_235240_a_(new BackgroundMusicSelector(SoundEvents.MUSIC_END, 6000, 12000, false)).
 								func_235238_a_())));
 		this.addCarver(GenerationStage.Carving.AIR, createCarver(WorldCarver.field_236240_b_, new ProbabilityConfig(0.2F)));
 	}
