d5871e4ac8ab6512b087644afbfc820c7e59de51
Tamaized <9671313+Tamaized@users.noreply.github.com>
2020-09-11 23:41:25 -0400

    fixed random seeds
    Signed-off-by: Tamaized <9671313+Tamaized@users.noreply.github.com>

--- src/main/java/tamaized/voidscape/world/VoidChunkGenerator.java
+++ src/main/java/tamaized/voidscape/world/VoidChunkGenerator.java
@@ -16,22 +16,28 @@ public class VoidChunkGenerator extends NoiseChunkGenerator {
 
 	public static final Codec<VoidChunkGenerator> codec = RecordCodecBuilder.create((p_236091_0_) -> p_236091_0_.
 			group(BiomeProvider.field_235202_a_.
-					fieldOf("biome_source").
-					forGetter(ChunkGenerator::getBiomeProvider), DimensionSettings.field_236098_b_.fieldOf("settings").
-					forGetter(VoidChunkGenerator::getDimensionSettings)).
+							fieldOf("biome_source").
+							forGetter(ChunkGenerator::getBiomeProvider),
+
+					Codec.LONG.
+							fieldOf("seed").orElse(new Random().nextLong()).
+							forGetter(gen -> gen.seed),
+
+					DimensionSettings.field_236098_b_.
+							fieldOf("settings").
+							forGetter(VoidChunkGenerator::getDimensionSettings)).
 			apply(p_236091_0_, p_236091_0_.stable(VoidChunkGenerator::new)));
+
+	private long seed;
 
 	private VoidChunkGenerator(BiomeProvider biomeProvider1, long seed, Supplier<DimensionSettings> dimensionSettings) {
 		super(biomeProvider1, seed, dimensionSettings);
+		this.seed = seed;
 		int horizontalNoiseGranularity = dimensionSettings.get().func_236113_b_().func_236174_e_() * 2;
 		ObfuscationReflectionHelper.setPrivateValue(NoiseChunkGenerator.class, this, horizontalNoiseGranularity, "field_222564_k");
 		int noise = 16 / horizontalNoiseGranularity;
 		ObfuscationReflectionHelper.setPrivateValue(NoiseChunkGenerator.class, this, noise, "field_222565_l");
 		ObfuscationReflectionHelper.setPrivateValue(NoiseChunkGenerator.class, this, noise, "field_222567_n");
-	}
-
-	private VoidChunkGenerator(BiomeProvider biomeProvider, Supplier<DimensionSettings> dimensionSettings) {
-		this(biomeProvider, new Random().nextLong(), dimensionSettings);
 	}
 
 	@Override
