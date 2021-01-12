20437322c952d6a4be4d31d286b3b80ae66c0577
Tamaized <9671313+Tamaized@users.noreply.github.com>
2021-01-12 10:42:04 -0500

    finished the join other party screen
    Signed-off-by: Tamaized <9671313+Tamaized@users.noreply.github.com>

--- src/main/java/tamaized/voidscape/client/ui/screen/PartyListScreen.java
+++ src/main/java/tamaized/voidscape/client/ui/screen/PartyListScreen.java
@@ -13,6 +13,7 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.text.TranslationTextComponent;
 import org.lwjgl.opengl.GL11;
 import tamaized.voidscape.Voidscape;
+import tamaized.voidscape.network.server.ServerPacketRequestJoinParty;
 import tamaized.voidscape.network.server.ServerPacketRequestPartyList;
 import tamaized.voidscape.party.ClientPartyInfo;
 import tamaized.voidscape.turmoil.Duties;
@@ -25,8 +26,8 @@ public class PartyListScreen extends TurmoilScreen {
 
 	private final Duties.Duty duty;
 	private final Instance.InstanceType type;
-	private boolean initedOnce;
 	public boolean joining;
+	private boolean initedOnce;
 	private Button join;
 	private TextFieldWidget password;
 	private PartyList list;
@@ -91,20 +92,23 @@ public class PartyListScreen extends TurmoilScreen {
 				new TranslationTextComponent("Join Party"),
 
 				button -> {
+					if (list.getSelected() == null)
+						return;
 					joining = true;
 					join.active = false;
 					password.active = false;
+					Voidscape.NETWORK.sendToServer(new ServerPacketRequestJoinParty(list.getSelected().party.host.getId(), password.getValue()));
 				}
 
 		));
 		join.active = false;
-		addWidget(password = new TextFieldWidget(
+		password = new TextFieldWidget(
 
 				font,
 
 				(int) (window.getGuiScaledWidth() / 4F - buttonWidth / 2F),
 
-				(int) (window.getGuiScaledHeight() / 4F - buttonHeight / 2F) + spacingHeight,
+				(int) ((window.getGuiScaledHeight() / 4F - buttonHeight / 2F) + spacingHeight + font.lineHeight + 2),
 
 				buttonWidth,
 
@@ -112,7 +116,8 @@ public class PartyListScreen extends TurmoilScreen {
 
 				new TranslationTextComponent("Password")
 
-		));
+		);
+		children.add(password);
 		addButton(new Button(
 
 				(int) (window.getGuiScaledWidth() / 2F - buttonWidth / 2F),
@@ -134,9 +139,20 @@ public class PartyListScreen extends TurmoilScreen {
 	public void tick() {
 		if (minecraft != null && minecraft.player != null && minecraft.player.tickCount % (20 * 5) == 0)
 			Voidscape.NETWORK.sendToServer(new ServerPacketRequestPartyList());
+		password.tick();
 		list.children().stream().filter(entry -> !ClientPartyInfo.PARTIES.contains(entry.party)).collect(Collectors.toList()).forEach(list::removeEntry);
 		ClientPartyInfo.PARTIES.stream().filter(party -> list.children().stream().noneMatch(entry -> entry.party == party)).forEach(party -> list.children().add(new PartyList.Entry(party)));
 		join.active = !joining && list.getSelected() != null;
+	}
+
+	@Override
+	public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+		return super.keyPressed(keyCode, scanCode, modifiers) || this.password.keyPressed(keyCode, scanCode, modifiers);
+	}
+
+	@Override
+	public boolean charTyped(char codePoint, int modifiers) {
+		return super.charTyped(codePoint, modifiers) || this.password.charTyped(codePoint, modifiers);
 	}
 
 	@Override
@@ -151,21 +167,24 @@ public class PartyListScreen extends TurmoilScreen {
 		}
 		super.render(matrixStack, mouseX, mouseY, partialTicks);
 		list.render(matrixStack, mouseX, mouseY, partialTicks);
+		password.render(matrixStack, mouseX, mouseY, partialTicks);
+		String text = new TranslationTextComponent("Password:").getString();
+		font.draw(matrixStack, text, (int) (minecraft.getWindow().getGuiScaledWidth() / 4 - font.width(text) / 2F), (int) (minecraft.getWindow().getGuiScaledHeight() / 4F - 20F / 2F) + 30F, 0xFFFFFFFF);
 	}
 
 	static class PartyList extends ExtendedList<PartyList.Entry> {
+
+		public PartyList(Minecraft mcIn, int widthIn, int heightIn, int topIn, int bottomIn, int slotHeightIn) {
+			super(mcIn, widthIn, heightIn, topIn, bottomIn, slotHeightIn);
+			setRenderBackground(false);
+			setRenderTopAndBottom(false);
+		}
 
 		private static void quad(BufferBuilder buffer, float x, float y, float z, float w, float h, float r, float g, float b, float a) {
 			buffer.vertex(x, y + h, z).color(r, g, b, a).uv(0F, 1F).endVertex();
 			buffer.vertex(x + w, y + h, z).color(r, g, b, a).uv(1F, 1F).endVertex();
 			buffer.vertex(x + w, y, z).color(r, g, b, a).uv(1F, 0F).endVertex();
 			buffer.vertex(x, y, z).color(r, g, b, a).uv(0F, 0F).endVertex();
-		}
-
-		public PartyList(Minecraft mcIn, int widthIn, int heightIn, int topIn, int bottomIn, int slotHeightIn) {
-			super(mcIn, widthIn, heightIn, topIn, bottomIn, slotHeightIn);
-			setRenderBackground(false);
-			setRenderTopAndBottom(false);
 		}
 
 		@Override
