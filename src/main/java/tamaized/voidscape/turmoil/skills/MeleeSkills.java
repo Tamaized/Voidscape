ad51d91986a2a4a97e9037c3d27697d354c6f252
Tamaized <9671313+Tamaized@users.noreply.github.com>
2021-02-23 13:25:26 -0500

    melee skills
    Signed-off-by: Tamaized <9671313+Tamaized@users.noreply.github.com>

--- src/main/java/tamaized/voidscape/turmoil/skills/MeleeSkills.java
+++ src/main/java/tamaized/voidscape/turmoil/skills/MeleeSkills.java
@@ -4,6 +4,74 @@ public class MeleeSkills implements ITurmoilSkills {
 
 	public final TurmoilSkill CORE = builder("core").core().build();
 
+	public final TurmoilSkill VOIDIC_BOND = builder("voidic_bond").required(CORE).stats(stats -> stats.voidicDamage(1).rechargeRate(1)).build();
+	public final TurmoilSkill VOIDIC_DAMAGE_1 = builder("voidic_damage_1").required(VOIDIC_BOND).stats(stats -> stats.voidicDamage(1)).build();
+	public final TurmoilSkill VOIDIC_DAMAGE_2 = builder("voidic_damage_2").required(VOIDIC_DAMAGE_1).stats(stats -> stats.voidicDamage(1)).build();
+	public final TurmoilSkill VOIDIC_DAMAGE_3 = builder("voidic_damage_3").required(VOIDIC_DAMAGE_2).stats(stats -> stats.voidicDamage(1)).build();
+	public final TurmoilSkill VOIDIC_DAMAGE_4 = builder("voidic_damage_4").required(VOIDIC_DAMAGE_3).stats(stats -> stats.voidicDamage(2)).build();
+	public final TurmoilSkill VOIDIC_DAMAGE_5 = builder("voidic_damage_5").required(VOIDIC_DAMAGE_4).stats(stats -> stats.voidicDamage(5)).build();
+	public final TurmoilSkill BONDING_1 = builder("bonding_1").required(VOIDIC_BOND).stats(stats -> stats.rechargeRate(1)).build();
+	public final TurmoilSkill BONDING_2 = builder("bonding_2").required(BONDING_1).stats(stats -> stats.rechargeRate(1)).build();
+	public final TurmoilSkill BONDING_3 = builder("bonding_3").required(BONDING_2).stats(stats -> stats.rechargeRate(1)).build();
+	public final TurmoilSkill BONDING_4 = builder("bonding_4").required(BONDING_3).stats(stats -> stats.rechargeRate(2)).build();
+	public final TurmoilSkill BONDING_5 = builder("bonding_5").required(BONDING_4).stats(stats -> stats.rechargeRate(4)).build();
+	public final TurmoilSkill SPELL_DEX_1 = builder("spell_dex_1").required(VOIDIC_BOND).stats(stats -> stats.cooldown(5)).build();
+	public final TurmoilSkill SPELL_DEX_2 = builder("spell_dex_2").required(SPELL_DEX_1).stats(stats -> stats.cooldown(5)).build();
+	public final TurmoilSkill SPELL_DEX_3 = builder("spell_dex_3").required(SPELL_DEX_2).stats(stats -> stats.cooldown(5)).build();
+	public final TurmoilSkill SPELL_DEX_4 = builder("spell_dex_4").required(SPELL_DEX_3).stats(stats -> stats.cooldown(5)).build();
+	public final TurmoilSkill SPELL_DEX_5 = builder("spell_dex_5").required(SPELL_DEX_4).stats(stats -> stats.cooldown(10)).build();
+	public final TurmoilSkill ASTUTE_UNDERSTANDING_1 = builder("astute_understanding_1").required(VOIDIC_BOND).stats(stats -> stats.cost(1)).build();
+	public final TurmoilSkill ASTUTE_UNDERSTANDING_2 = builder("astute_understanding_2").required(ASTUTE_UNDERSTANDING_1).stats(stats -> stats.cost(1)).build();
+	public final TurmoilSkill ASTUTE_UNDERSTANDING_3 = builder("astute_understanding_3").required(ASTUTE_UNDERSTANDING_2).stats(stats -> stats.cost(1)).build();
+	public final TurmoilSkill ASTUTE_UNDERSTANDING_4 = builder("astute_understanding_4").required(ASTUTE_UNDERSTANDING_3).stats(stats -> stats.cost(2)).build();
+	public final TurmoilSkill ASTUTE_UNDERSTANDING_5 = builder("astute_understanding_5").required(ASTUTE_UNDERSTANDING_4).stats(stats -> stats.cost(5)).build();
+
+	public final TurmoilSkill VOIDIC_FIGHTER_1 = builder("voidic_fighter_1").required(CORE).stats(stats -> stats.voidicDamage(1).voidicDamageReduction(1)).build();
+	public final TurmoilSkill WAY_OF_THE_BLADE_1 = builder("way_of_the_blade_1").required(VOIDIC_FIGHTER_1).stats(stats -> stats.voidicDamage(1)).build();
+	public final TurmoilSkill EMPOWER_ATTACK_SLICING = builder("empower_attack_slicing").required(VOIDIC_FIGHTER_1).build();
+	public final TurmoilSkill VOIDIC_FIGHTER_2 = builder("voidic_fighter_2").required(VOIDIC_FIGHTER_1).stats(stats -> stats.voidicDamage(1).voidicDamageReduction(1)).build();
+	public final TurmoilSkill WAY_OF_THE_BLADE_2 = builder("way_of_the_blade_2").required(VOIDIC_FIGHTER_2, WAY_OF_THE_BLADE_1).stats(stats -> stats.voidicDamage(1)).build();
+	public final TurmoilSkill EMPOWER_ATTACK_2X = builder("empower_attack_2x").required(VOIDIC_FIGHTER_2).build();
+	public final TurmoilSkill VOIDIC_FIGHTER_3 = builder("voidic_fighter_3").required(VOIDIC_FIGHTER_2).stats(stats -> stats.voidicDamage(1).voidicDamageReduction(1)).build();
+	public final TurmoilSkill WAY_OF_THE_BLADE_3 = builder("way_of_the_blade_3").required(VOIDIC_FIGHTER_3, WAY_OF_THE_BLADE_2).stats(stats -> stats.voidicDamage(1)).build();
+	public final TurmoilSkill EMPOWER_ATTACK_LIFESTEAL = builder("empower_attack_lifesteal").required(VOIDIC_FIGHTER_3).build();
+	public final TurmoilSkill VOIDIC_FIGHTER_4 = builder("voidic_fighter_4").required(VOIDIC_FIGHTER_3).stats(stats -> stats.voidicDamage(2).voidicDamageReduction(2)).build();
+	public final TurmoilSkill WAY_OF_THE_BLADE_4 = builder("way_of_the_blade_4").required(VOIDIC_FIGHTER_4, WAY_OF_THE_BLADE_3).stats(stats -> stats.voidicDamage(2)).build();
+	public final TurmoilSkill EMPOWER_ATTACK_3X = builder("empower_attack_3x").required(VOIDIC_FIGHTER_4).build();
+	public final TurmoilSkill VOIDIC_FIGHTER_5 = builder("voidic_fighter_5").required(VOIDIC_FIGHTER_4).stats(stats -> stats.voidicDamage(5).voidicDamageReduction(5)).build();
+	public final TurmoilSkill WAY_OF_THE_BLADE_5 = builder("way_of_the_blade_5").required(VOIDIC_FIGHTER_5, WAY_OF_THE_BLADE_4).stats(stats -> stats.voidicDamage(5)).build();
+	public final TurmoilSkill BLITZ = builder("blitz").required(VOIDIC_FIGHTER_5).build();
+
+	public final TurmoilSkill TEMPEST_1 = builder("tempest_1").required(CORE).stats(stats -> stats.voidicDamage(1)).build();
+	public final TurmoilSkill DEFT_STRIKES_1 = builder("deft_strikes_1").required(TEMPEST_1).stats(stats -> stats.cooldown(1)).build();
+	public final TurmoilSkill CLEAVE = builder("cleave").required(TEMPEST_1).build();
+	public final TurmoilSkill TEMPEST_2 = builder("tempest_2").required(TEMPEST_1).stats(stats -> stats.voidicDamage(1)).build();
+	public final TurmoilSkill DEFT_STRIKES_2 = builder("deft_strikes_2").required(TEMPEST_2).stats(stats -> stats.cooldown(1)).build();
+	public final TurmoilSkill GREATER_CLEAVE = builder("greater_cleave").required(TEMPEST_2).build();
+	public final TurmoilSkill TEMPEST_3 = builder("tempest_3").required(TEMPEST_2).stats(stats -> stats.voidicDamage(1)).build();
+	public final TurmoilSkill DEFT_STRIKES_3 = builder("deft_strikes_3").required(TEMPEST_3).stats(stats -> stats.voidicDamage(1)).build();
+	public final TurmoilSkill WHIRLWIND = builder("whirlwind").required(TEMPEST_3).build();
+	public final TurmoilSkill TEMPEST_4 = builder("tempest_4").required(TEMPEST_3).stats(stats -> stats.voidicDamage(2)).build();
+	public final TurmoilSkill DEFT_STRIKES_4 = builder("deft_strikes_4").required(TEMPEST_4).stats(stats -> stats.cooldown(2)).build();
+	public final TurmoilSkill TWO_WEAPON_FIGHTING = builder("two_weapon_fighting").required(TEMPEST_4).build();
+	public final TurmoilSkill TEMPEST_5 = builder("tempest_5").required(TEMPEST_4).stats(stats -> stats.voidicDamage(5)).build();
+	public final TurmoilSkill DEFT_STRIKES_5 = builder("deft_strikes_5").required(TEMPEST_5).stats(stats -> stats.cooldown(5)).build();
+	public final TurmoilSkill WHIRLING_BLADES = builder("whirling_blades").required(TEMPEST_5).build();
+
+	public final TurmoilSkill CHAOS_BLADE_1 = builder("chaos_blade_1").required(CORE).stats(stats -> stats.voidicDamage(1)).build();
+	public final TurmoilSkill EMPOWER_ATTACK_BLEED = builder("empower_attack_bleed").required(CHAOS_BLADE_1).build();
+	public final TurmoilSkill SENSE_WEAKNESS = builder("sense_weakness").required(CHAOS_BLADE_1).build();
+	public final TurmoilSkill CHAOS_BLADE_2 = builder("chaos_blade_2").required(CHAOS_BLADE_1).stats(stats -> stats.voidicDamage(1)).build();
+	public final TurmoilSkill REJUVENATE = builder("rejuvenate").required(CHAOS_BLADE_2).build();
+	public final TurmoilSkill EMPOWER_ATTACK_SLASH = builder("empower_attack_slash").required(CHAOS_BLADE_2).build();
+	public final TurmoilSkill CHAOS_BLADE_3 = builder("chaos_blade_3").required(CHAOS_BLADE_2).stats(stats -> stats.voidicDamage(1)).build();
+	public final TurmoilSkill OVERWHELMING_FORCE = builder("overwhelming_force").required(CHAOS_BLADE_3).build();
+	public final TurmoilSkill SHARP_EDGE = builder("sharp_edge").required(CHAOS_BLADE_3).build();
+	public final TurmoilSkill CHAOS_BLADE_4 = builder("chaos_blade_4").required(CHAOS_BLADE_3).stats(stats -> stats.voidicDamage(2)).build();
+	public final TurmoilSkill BLOODBATH = builder("bloodbath").required(CHAOS_BLADE_4).build();
+	public final TurmoilSkill CHAOS_BLADE_5 = builder("chaos_blade_5").required(CHAOS_BLADE_4).stats(stats -> stats.voidicDamage(5)).build();
+	public final TurmoilSkill PRIMAL_RAGE = builder("primal_rage").required(CHAOS_BLADE_5).build();
+
 	@Override
 	public String location() {
 		return "melee";
