2ecad41e072ad69f9fd3dc5200924d95fbc1f360
Tamaized <9671313+Tamaized@users.noreply.github.com>
2021-05-30 11:55:08 -0400

    localize Talk text
    Signed-off-by: Tamaized <9671313+Tamaized@users.noreply.github.com>

--- src/main/java/tamaized/voidscape/turmoil/Talk.java
+++ src/main/java/tamaized/voidscape/turmoil/Talk.java
@@ -13,78 +13,17 @@ import java.util.function.Consumer;
 public class Talk {
 
 	public static final ResourceLocation FORMAT_KEYBIND = new ResourceLocation(Voidscape.MODID, "keybind");
+	public static final String KEY_BASE = Voidscape.MODID + ".talk.entry.";
 
 	public static Entry INTRO = new Entry(new ResourceLocation(Voidscape.MODID, "intro"),
 
-			new TranslationTextComponent(
-
-					"(Press [%1$s])\n" +
-
-							"Do not be afraid.\n" +
-
-							"I am you.\n" +
-
-							"As you are me.\n" +
-
-							"We are one the same.\n" +
-
-							"This is just a nightmare.\n" +
-
-							"We are the nightmare.\n" +
-
-							"We seek power.\n" +
-
-							"We can gain power.\n" +
-
-							"We will guide each other through oblivion to gain such power.\n" +
-
-							"Power greater than that of eld.\n" +
-
-							"You are the Blade.\n" +
-
-							"I am the Helm.\n" +
-
-							"Together we shall rise above all.\n" +
-
-							"Do we trust one another?\n" +
-
-							"Let the Void consume us.\n" +
-
-							"We're heading to oblivion.\n" +
-
-							"Let the nightmare begin.\n" +
-
-							"(Everything past this point is heavily a work in progress)\n" +
-
-							"(To exit the dimension you must die, you will not lose your inventory)\n" +
-
-							"(You may enter at any time with [%1$s])",
-
-					format(FORMAT_KEYBIND)),
+			new TranslationTextComponent(KEY_BASE + "intro", format(FORMAT_KEYBIND)),
 
 			(host) -> host.getCapability(SubCapability.CAPABILITY).ifPresent(cap -> cap.get(Voidscape.subCapTurmoilData).ifPresent(Turmoil::start)));
 
 	public static Entry TUTORIAL = new Entry(new ResourceLocation(Voidscape.MODID, "tutorial"),
 
-			new TranslationTextComponent(
-
-					"Welcome to the Void\n" +
-
-							"Tread carefully, the landscape is very... chaotic\n" +
-
-							"We wouldn't want to fall into the abyss would we?\n" +
-
-							"Be aware, while a material mortal such as us treads in this ethereal land...\n" +
-
-							"Various effects will take place, both in Mind and Body\n" +
-
-							"While we linger, our essence is spread onto the rock beneath us\n" +
-
-							"This essence forms crystal. This crystal is useful for us\n" +
-
-							"Covering ourselves in it we can slow the voidic decay\n" +
-
-							"Move quickly before our body completely deteriorates!"),
+			new TranslationTextComponent(KEY_BASE + "tutorial"),
 
 			(host) -> host.getCapability(SubCapability.CAPABILITY).ifPresent(cap -> cap.get(Voidscape.subCapTurmoilData).ifPresent(data -> {
 				data.progressTo(Progression.EnteredVoid);
@@ -94,21 +33,7 @@ public class Talk {
 
 	public static Entry TUTORIAL_GUI = new Entry(new ResourceLocation(Voidscape.MODID, "tutorialgui"),
 
-			new TranslationTextComponent(
-
-					"This is where we can look into our inner turmoil\n" +
-
-							"We may call upon our turmoil to accomplish various tasks\n" +
-
-							"One such task is to have the Void consume us and take us back here at any time\n" +
-
-							"Another is the ability to shape our own power to our liking\n" +
-
-							"Let us shape our Voidic Powers now...\n" +
-
-							"(Press [%1$s] to open the UI at any time)",
-
-					format(FORMAT_KEYBIND)),
+			new TranslationTextComponent(KEY_BASE + "tutorialgui", format(FORMAT_KEYBIND)),
 
 			(host) -> host.getCapability(SubCapability.CAPABILITY).ifPresent(cap -> cap.get(Voidscape.subCapTurmoilData).ifPresent(data -> {
 				data.progressTo(Progression.MidTutorial);
@@ -116,33 +41,7 @@ public class Talk {
 
 	public static Entry TUTORIAL_SKILLS = new Entry(new ResourceLocation(Voidscape.MODID, "tutorialskills"),
 
-			new TranslationTextComponent(
-
-					"Here we can shape our power how we wish\n" +
-
-							"We can only go down a single path at a time\n" +
-
-							"Let us make our choices wisely\n" +
-
-							"You may however allow the void to reshape us\n" +
-
-							"With a Voidic Crystal in possession, allow your turmoil to shatter it\n" +
-
-							"This can only be done once an hour\n" +
-
-							"(This entire section is a majorly work in progress)\n" +
-
-							"(You can only claim one skill)\n" +
-
-							"(This skill comes with an ability)\n" +
-
-							"(You must slot this ability into your spell bar)\n" +
-
-							"(You can find the spell bar configuration on the main ui)\n" +
-
-							"(Simply click, hold, and drag the ability onto any slot on the bar)\n" +
-
-							"(Check Controls for what keybinds to use or configure in order to use the spell bar)"),
+			new TranslationTextComponent(KEY_BASE + "tutorialskills"),
 
 			(host) -> host.getCapability(SubCapability.CAPABILITY).ifPresent(cap -> cap.get(Voidscape.subCapTurmoilData).ifPresent(data -> {
 				data.progressTo(Progression.PostTutorial);
@@ -150,27 +49,7 @@ public class Talk {
 
 	public static Entry CORRUPT_PHANTOM = new Entry(new ResourceLocation(Voidscape.MODID, "corruptphantom"),
 
-			new TranslationTextComponent(
-
-					"The Corrupted Pawn\n" +
-
-							"One of Xia's misguided henchmen\n" +
-
-							"We only bore witness to its Phantom\n" +
-
-							"It uses this form to banish outsiders back to their own Realm\n" +
-
-							"It may only manifest in the minds of complete insanity\n" +
-
-							"Infact its a great contributor to the maddening effects of the Void\n" +
-
-							"Slay its true form and we may explore further in the Void\n" +
-
-							"Our Turmoil can take us directly to the beast\n" +
-
-							"This wont be easy, come prepared\n " +
-
-							"Let us improve our Voidic Powers first..."),
+			new TranslationTextComponent(KEY_BASE + "corruptphantom"),
 
 			(host) -> host.getCapability(SubCapability.CAPABILITY).ifPresent(cap -> cap.get(Voidscape.subCapTurmoilData).ifPresent(data -> {
 				data.progressTo(Progression.CorruptPawnPre);
@@ -179,29 +58,7 @@ public class Talk {
 
 	public static Entry CORRUPT_PAWN = new Entry(new ResourceLocation(Voidscape.MODID, "corruptpawn"),
 
-			new TranslationTextComponent(
-
-					"We've done it...\n" +
-
-							"The Pawn is slain.\n" +
-
-							"No longer will we have to deal with the enhanced paranoia.\n" +
-
-							"Of course we will still experience insanity.\n" +
-
-							"Just not to the extreme as we were...\n" +
-
-							"Now then, lets use the charred bone of our dead enemy.\n" +
-
-							"It serves more purpose than just a trophy however.\n" +
-
-							"It will further reduce Voidic Infusion within the Void.\n" +
-
-							"Not only that, the helm helps us see in the dark Void.\n" +
-
-							"Let us use this to venture into the Tower of Null.\n" +
-
-							"(Note: The Tower of Null does not exist yet!)"),
+			new TranslationTextComponent(KEY_BASE + "corruptpawn"),
 
 			(host) -> host.getCapability(SubCapability.CAPABILITY).ifPresent(cap -> cap.get(Voidscape.subCapTurmoilData).ifPresent(data -> {
 				data.progressTo(Progression.CorruptPawnPost);
