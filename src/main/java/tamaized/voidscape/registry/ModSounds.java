b7bec1cc2415bbc31a80c74bc043ab2e589ec0d3
Tamaized <9671313+Tamaized@users.noreply.github.com>
2021-10-26 23:14:24 -0400

    add nether music
    Signed-off-by: Tamaized <9671313+Tamaized@users.noreply.github.com>

--- src/main/java/tamaized/voidscape/registry/ModSounds.java
+++ src/main/java/tamaized/voidscape/registry/ModSounds.java
@@ -9,6 +9,7 @@ import tamaized.voidscape.Voidscape;
 public class ModSounds {
 
 	public static final RegistryObject<SoundEvent> AMBIENCE = RegUtil.create(ForgeRegistries.SOUND_EVENTS).register("ambience", () -> new SoundEvent(new ResourceLocation(Voidscape.MODID, "ambience")));
+	public static final RegistryObject<SoundEvent> MUSIC = RegUtil.create(ForgeRegistries.SOUND_EVENTS).register("music", () -> new SoundEvent(new ResourceLocation(Voidscape.MODID, "music")));
 
 	static void classload() {
 
