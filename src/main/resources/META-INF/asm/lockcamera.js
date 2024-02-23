d88e347726f855d4d3fb46471d45d18e96f70a3b
Tamaized <9671313+Tamaized@users.noreply.github.com>
2024-02-23 18:53:31 -0500

    Cleanup Imports

--- src/main/resources/META-INF/asm/lockcamera.js
+++ src/main/resources/META-INF/asm/lockcamera.js
@@ -4,7 +4,6 @@ var ASM = Java.type('net.neoforged.coremod.api.ASMAPI');
 var Opcodes = Java.type('org.objectweb.asm.Opcodes');
 
 var MethodInsnNode = Java.type('org.objectweb.asm.tree.MethodInsnNode');
-var VarInsnNode = Java.type('org.objectweb.asm.tree.VarInsnNode');
 
 // noinspection JSUnusedGlobalSymbols
 function initializeCoreMod() {
