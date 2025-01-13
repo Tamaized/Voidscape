c17914af95df687a2867260e4b25a33c46c482ce
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-01-12 20:09:55 -0500

    Port EntityLayerRendererRegistration

---
+++ src/main/java/tamaized/voidscape/client/event/EntityLayerRendererRegistration.java
@@ -0,0 +1,27 @@
+package tamaized.voidscape.client.event;
+
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import tamaized.beanification.Component;
+import tamaized.beanification.PostConstruct;
+import tamaized.voidscape.client.DonatorLayer;
+
+@Component
+public class EntityLayerRendererRegistration {
+
+	@PostConstruct
+	private void setup(IEventBus bus) {
+		bus.addListener(EntityRenderersEvent.AddLayers.class, event -> {
+			event.getSkins().forEach(renderer -> {
+				LivingEntityRenderer<Player, EntityModel<Player>> skin = event.getSkin(renderer);
+				if (skin == null)
+					return;
+				skin.addLayer(new DonatorLayer<>(skin));
+			});
+		});
+	}
+
+}
