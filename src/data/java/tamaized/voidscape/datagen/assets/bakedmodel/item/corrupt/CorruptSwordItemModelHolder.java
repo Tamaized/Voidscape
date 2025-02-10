437d45e3323b357c06d37e162b32cdc9714107a9
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-02-09 23:54:48 -0500

    Corrupt gear item model datagen

---
+++ src/data/java/tamaized/voidscape/datagen/assets/bakedmodel/item/corrupt/CorruptSwordItemModelHolder.java
@@ -0,0 +1,59 @@
+package tamaized.voidscape.datagen.assets.bakedmodel.item.corrupt;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
+import net.neoforged.neoforge.client.model.generators.loaders.ItemLayerModelBuilder;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import org.jetbrains.annotations.Nullable;
+import tamaized.beanification.Autowired;
+import tamaized.beanification.Component;
+import tamaized.voidscape.client.ItemModelOverridePredicates;
+import tamaized.voidscape.datagen.assets.bakedmodel.ItemModelHolder;
+import tamaized.voidscape.registry.ModToolSetComponentDirectory;
+
+@Component
+public class CorruptSwordItemModelHolder extends ItemModelHolder {
+
+	@Autowired
+	private ModToolSetComponentDirectory tools;
+
+	@Autowired
+	private ItemModelOverridePredicates itemModelOverridePredicates;
+
+	@Nullable
+	private ModelFile brokenModel;
+
+	@Override
+	protected DeferredHolder<Item, ? extends Item> itemForName() {
+		return tools.corruptToolSet().CORRUPT_SWORD;
+	}
+
+	public ModelFile build(ItemModelProvider provider) {
+		// @formatter:off
+		return provider.withExistingParent(
+				splitName(),
+				"item/handheld"
+			)
+			.texture("layer0", "item/corrupt/sword")
+			.customLoader(ItemLayerModelBuilder::begin).emissive(15, 15, 0).end()
+			.override()
+				.predicate(itemModelOverridePredicates.BROKEN, 1)
+				.model(getBrokenModel(provider))
+			.end();
+		// @formatter:on
+	}
+
+	private ModelFile getBrokenModel(ItemModelProvider provider) {
+		if (brokenModel == null) {
+			brokenModel = provider.withExistingParent(
+					splitName("broken"),
+					"item/handheld"
+				)
+				.texture("layer0", "item/corrupt/sword_broken")
+				.customLoader(ItemLayerModelBuilder::begin).emissive(15, 15, 0).end();
+		}
+		return brokenModel;
+	}
+
+}
