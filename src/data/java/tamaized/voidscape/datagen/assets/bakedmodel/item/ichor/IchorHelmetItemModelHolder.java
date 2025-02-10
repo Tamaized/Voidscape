3e23ff1ebbded5f2004f1cffa32dc17218ad7f09
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-02-10 00:01:07 -0500

    Ichor gear item model datagen

---
+++ src/data/java/tamaized/voidscape/datagen/assets/bakedmodel/item/ichor/IchorHelmetItemModelHolder.java
@@ -0,0 +1,59 @@
+package tamaized.voidscape.datagen.assets.bakedmodel.item.ichor;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
+import net.neoforged.neoforge.client.model.generators.loaders.ItemLayerModelBuilder;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import org.jetbrains.annotations.Nullable;
+import tamaized.beanification.Autowired;
+import tamaized.beanification.Component;
+import tamaized.voidscape.client.ItemModelOverridePredicates;
+import tamaized.voidscape.datagen.assets.bakedmodel.ItemModelHolder;
+import tamaized.voidscape.registry.ModArmorSetComponentDirectory;
+
+@Component
+public class IchorHelmetItemModelHolder extends ItemModelHolder {
+
+	@Autowired
+	private ModArmorSetComponentDirectory armor;
+
+	@Autowired
+	private ItemModelOverridePredicates itemModelOverridePredicates;
+
+	@Nullable
+	private ModelFile brokenModel;
+
+	@Override
+	protected DeferredHolder<Item, ? extends Item> itemForName() {
+		return armor.ichorArmorSet().ICHOR_HELMET;
+	}
+
+	public ModelFile build(ItemModelProvider provider) {
+		// @formatter:off
+		return provider.withExistingParent(
+				splitName(),
+				"item/generated"
+			)
+			.texture("layer0", "item/ichor/helmet")
+			.customLoader(ItemLayerModelBuilder::begin).emissive(15, 15, 0).end()
+			.override()
+				.predicate(itemModelOverridePredicates.BROKEN, 1)
+				.model(getBrokenModel(provider))
+			.end();
+		// @formatter:on
+	}
+
+	public ModelFile getBrokenModel(ItemModelProvider provider) {
+		if (brokenModel == null) {
+			brokenModel = provider.withExistingParent(
+					splitName("broken"),
+					"item/generated"
+				)
+				.texture("layer0", "item/ichor/helmet_broken")
+				.customLoader(ItemLayerModelBuilder::begin).emissive(15, 15, 0).end();
+		}
+		return brokenModel;
+	}
+
+}
---
+++ src/data/java/tamaized/voidscape/datagen/assets/bakedmodel/item/ichor/package-info.java
@@ -0,0 +1,6 @@
+@MethodsReturnNonnullByDefault @ParametersAreNonnullByDefault
+package tamaized.voidscape.datagen.assets.bakedmodel.item.ichor;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
