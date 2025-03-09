0db44d6303007c236c7382189e9a96d3f73eb184
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-03-09 00:25:34 -0500

    NeedsDiamondToolTagProviderFactory and NeedsIronToolTagProviderFactory

---
+++ src/data/java/tamaized/voidscape/datagen/data/tag/block/NeedsDiamondToolTagProviderFactory.java
@@ -0,0 +1,27 @@
+package tamaized.voidscape.datagen.data.tag.block;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.tags.BlockTags;
+import tamaized.beanification.Autowired;
+import tamaized.beanification.Component;
+import tamaized.voidscape.datagen.RegistryProvider;
+import tamaized.voidscape.registry.ModBlockComponentDirectory;
+
+@Component
+public class NeedsDiamondToolTagProviderFactory implements IBlockTagProviderFactory {
+
+	@Autowired
+	private RegistryProvider registryProvider;
+
+	@Autowired
+	private ModBlockComponentDirectory blocks;
+
+	@Override
+	public void make(BlockTagProviderFactory.BlockTagsProviderAccessor accessor, HolderLookup.Provider provider) {
+		accessor.tag(BlockTags.NEEDS_DIAMOND_TOOL).add(
+			blocks.oreBlocks().VOIDIC_CRYSTAL_ORE.get(),
+			blocks.materialBlocks().VOIDIC_CRYSTAL_BLOCK.get()
+		);
+	}
+
+}
---
+++ src/data/java/tamaized/voidscape/datagen/data/tag/block/NeedsIronToolTagProviderFactory.java
@@ -0,0 +1,32 @@
+package tamaized.voidscape.datagen.data.tag.block;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.tags.BlockTags;
+import tamaized.beanification.Autowired;
+import tamaized.beanification.Component;
+import tamaized.voidscape.datagen.RegistryProvider;
+import tamaized.voidscape.registry.ModBlockComponentDirectory;
+
+@Component
+public class NeedsIronToolTagProviderFactory implements IBlockTagProviderFactory {
+
+	@Autowired
+	private RegistryProvider registryProvider;
+
+	@Autowired
+	private ModBlockComponentDirectory blocks;
+
+	@Override
+	public void make(BlockTagProviderFactory.BlockTagsProviderAccessor accessor, HolderLookup.Provider provider) {
+		accessor.tag(BlockTags.NEEDS_IRON_TOOL).add(
+			blocks.machineBlocks().MACHINE_LIQUIFIER.get(),
+			blocks.machineBlocks().MACHINE_DEFUSER.get(),
+			blocks.machineBlocks().MACHINE_GERMINATOR.get(),
+			blocks.machineBlocks().MACHINE_WELL.get(),
+			blocks.machineBlocks().MACHINE_COOP.get(),
+			blocks.machineBlocks().MACHINE_INFUSER.get(),
+			blocks.machineBlocks().MACHINE_COLLECTOR.get()
+		);
+	}
+
+}
