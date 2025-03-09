d17bf7367456f80026b1effe34b6a6aa8f46ebd0
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-03-08 23:47:17 -0500

    DragonImmuneTagProviderFactory

---
+++ src/data/java/tamaized/voidscape/datagen/data/tag/block/DragonImmuneTagProviderFactory.java
@@ -0,0 +1,33 @@
+package tamaized.voidscape.datagen.data.tag.block;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.tags.BlockTags;
+import tamaized.beanification.Autowired;
+import tamaized.beanification.Component;
+import tamaized.voidscape.datagen.RegistryProvider;
+import tamaized.voidscape.registry.ModBlockComponentDirectory;
+
+@Component
+public class DragonImmuneTagProviderFactory implements IBlockTagProviderFactory {
+
+	@Autowired
+	private RegistryProvider registryProvider;
+
+	@Autowired
+	private ModBlockComponentDirectory blocks;
+
+	@Override
+	public void make(BlockTagProviderFactory.BlockTagsProviderAccessor accessor, HolderLookup.Provider provider) {
+		accessor.tag(BlockTags.DRAGON_IMMUNE).add(
+			blocks.oreBlocks().VOIDIC_CRYSTAL_ORE.get(),
+			blocks.spireBlocks().THUNDERROCK.get(),
+			blocks.spireBlocks().ANTIROCK.get(),
+			blocks.nullBiomeBlocks().NULL_BLACK.get(),
+			blocks.nullBiomeBlocks().NULL_WHITE.get(),
+			blocks.spireBlocks().ASTRALROCK.get(),
+			blocks.thunderForestBiomeBlocks().THUNDER_NYLIUM.get(),
+			blocks.oreBlocks().CRACKED_ASTRALROCK.get()
+		);
+	}
+
+}
