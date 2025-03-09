f50d3561dc9ebcacb070bb8ea10b3706f076ff73
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-03-09 15:02:04 -0400

    ThunderStemsItemTagProviderFactory

---
+++ src/data/java/tamaized/voidscape/datagen/data/tag/item/ThunderStemsItemTagProviderFactory.java
@@ -0,0 +1,31 @@
+package tamaized.voidscape.datagen.data.tag.item;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import tamaized.beanification.Autowired;
+import tamaized.beanification.Component;
+import tamaized.voidscape.Voidscape;
+import tamaized.voidscape.datagen.RegistryProvider;
+import tamaized.voidscape.registry.ModBlockComponentDirectory;
+
+@Component
+public class ThunderStemsItemTagProviderFactory implements IItemTagProviderFactory {
+
+	@Autowired
+	private RegistryProvider registryProvider;
+
+	@Autowired
+	private ModBlockComponentDirectory blocks;
+
+	@Override
+	public void make(ItemTagProviderFactory.ItemTagsProviderAccessor accessor, HolderLookup.Provider provider) {
+		accessor.tag(TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Voidscape.MODID, "thunder_stems"))).add(
+			blocks.thunderForestBiomeBlocks().THUNDER_STEM_ITEM.get(),
+			blocks.thunderForestBiomeBlocks().THUNDER_HYPHAE_ITEM.get(),
+			blocks.thunderForestBiomeBlocks().THUNDER_STEM_STRIPPED_ITEM.get(),
+			blocks.thunderForestBiomeBlocks().THUNDER_HYPHAE_STRIPPED_ITEM.get()
+		);
+	}
+}
