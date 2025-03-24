8697a1d900460900881b90109c1ce05e51394b3a
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-03-24 09:51:20 -0400

    BlockLootTableUtil#ore

---
+++ src/data/java/tamaized/voidscape/datagen/data/loot/sub/block/FleshBlockBlockLootTableFactory.java
@@ -0,0 +1,39 @@
+package tamaized.voidscape.datagen.data.loot.sub.block;
+
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import tamaized.beanification.Autowired;
+import tamaized.beanification.Component;
+import tamaized.voidscape.datagen.RegistryProvider;
+import tamaized.voidscape.datagen.util.BlockLootTableUtil;
+import tamaized.voidscape.registry.ModBlockComponentDirectory;
+import tamaized.voidscape.registry.tool.ModItemComponentDirectory;
+
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
+@Component
+public class FleshBlockBlockLootTableFactory implements IBlockLootTable {
+
+	@Autowired
+	private RegistryProvider registries;
+
+	@Autowired
+	private BlockLootTableUtil blockLootTableUtil;
+
+	@Autowired
+	private ModBlockComponentDirectory blocks;
+
+	@Autowired
+	private ModItemComponentDirectory items;
+
+	public void add(BlockLootSubProvider provider, BiConsumer<Block, LootTable.Builder> add, Supplier<LootItemCondition.Builder> hasSilkTouch) {
+		add.accept(
+			blocks.materialBlocks().FLESH_BLOCK.get(),
+			provider.createSingleItemTable(blocks.materialBlocks().FLESH_BLOCK_ITEM.get())
+		);
+	}
+
+}
--- src/data/java/tamaized/voidscape/datagen/util/BlockLootTableUtil.java
+++ src/data/java/tamaized/voidscape/datagen/util/BlockLootTableUtil.java
@@ -1,17 +1,21 @@
 package tamaized.voidscape.datagen.util;
 
+import net.minecraft.advancements.critereon.*;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.AlternativesEntry;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
 import net.minecraft.world.level.storage.loot.predicates.ExplosionCondition;
+import net.minecraft.world.level.storage.loot.predicates.MatchTool;
 import tamaized.beanification.Autowired;
 import tamaized.beanification.Component;
 import tamaized.voidscape.datagen.RegistryProvider;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 @Component
@@ -22,15 +26,34 @@ public class BlockLootTableUtil {
 
 	public LootTable.Builder etherealFruit(Supplier<Item> fruit) {
 		return LootTable.lootTable().withPool(
-			LootPool.lootPool()
-				.add(
-					LootItem.lootTableItem(fruit.get())
-						.apply(ApplyBonusCount.addBonusBinomialDistributionCount(
-							registries.join().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.FORTUNE),
-							0.5714286F,
-							1
-						))
-				).when(ExplosionCondition.survivesExplosion())
+			LootPool.lootPool().add(
+				LootItem.lootTableItem(fruit.get())
+					.apply(ApplyBonusCount.addBonusBinomialDistributionCount(
+						registries.join().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.FORTUNE),
+						0.5714286F,
+						1
+					))
+			).when(ExplosionCondition.survivesExplosion())
+		);
+	}
+
+	public LootTable.Builder ore(Supplier<Item> item, Supplier<Item> itemFromSilkTouch) {
+		return LootTable.lootTable().withPool(
+			LootPool.lootPool().add(
+				AlternativesEntry.alternatives(
+					LootItem.lootTableItem(itemFromSilkTouch.get())
+						.when(MatchTool.toolMatches(ItemPredicate.Builder.item().withSubPredicate(
+							ItemSubPredicates.ENCHANTMENTS,
+							ItemEnchantmentsPredicate.enchantments(List.of(
+								new EnchantmentPredicate(
+									registries.join().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.SILK_TOUCH),
+									MinMaxBounds.Ints.ANY
+								)
+							))
+						))),
+					LootItem.lootTableItem(item.get())
+				)
+			).when(ExplosionCondition.survivesExplosion())
 		);
 	}
 
