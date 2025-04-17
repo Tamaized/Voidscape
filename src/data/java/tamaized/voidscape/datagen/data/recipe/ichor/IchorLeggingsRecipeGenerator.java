a1b3564b8cad0875a1702cbd6fb20af339953051
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-04-16 22:38:45 -0400

    Ichor recipe datagen

---
+++ src/data/java/tamaized/voidscape/datagen/data/recipe/ichor/IchorLeggingsRecipeGenerator.java
@@ -0,0 +1,35 @@
+package tamaized.voidscape.datagen.data.recipe.ichor;
+
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.SmithingTransformRecipeBuilder;
+import net.minecraft.world.item.crafting.Ingredient;
+import tamaized.beanification.Autowired;
+import tamaized.beanification.Component;
+import tamaized.voidscape.datagen.data.recipe.IRecipeGenerator;
+import tamaized.voidscape.datagen.util.RecipeProviderUtil;
+import tamaized.voidscape.registry.tool.ModItemComponentDirectory;
+
+@Component
+public class IchorLeggingsRecipeGenerator implements IRecipeGenerator {
+
+	@Autowired
+	private ModItemComponentDirectory items;
+
+	@Autowired
+	private RecipeProviderUtil recipeProviderUtil;
+
+	@Override
+	public void generate(RecipeOutput recipeOutput) {
+		SmithingTransformRecipeBuilder.smithing(
+			Ingredient.of(items.augmentItems().VOIDIC_TEMPLATE.get()),
+			Ingredient.of(items.modArmorSetComponentDirectory().titaniteArmorSet().TITANITE_LEGS.get()),
+			Ingredient.of(items.materialItems().ICHOR_CRYSTAL.get()),
+			RecipeCategory.COMBAT,
+			items.modArmorSetComponentDirectory().ichorArmorSet().ICHOR_LEGS.get()
+		)
+			.unlocks("has_template", recipeProviderUtil.has(items.augmentItems().VOIDIC_TEMPLATE.get()))
+			.save(recipeOutput, "ichor_leggings");
+	}
+
+}
