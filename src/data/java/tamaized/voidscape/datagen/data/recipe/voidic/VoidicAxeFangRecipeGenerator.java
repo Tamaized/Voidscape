338bba71fa954edd68469c558241aad9088e4643
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-04-16 23:00:10 -0400

    Voidic Crystal recipe datagen

---
+++ src/data/java/tamaized/voidscape/datagen/data/recipe/voidic/VoidicAxeFangRecipeGenerator.java
@@ -0,0 +1,42 @@
+package tamaized.voidscape.datagen.data.recipe.voidic;
+
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import tamaized.beanification.Autowired;
+import tamaized.beanification.Component;
+import tamaized.voidscape.datagen.data.recipe.ExtendedSmithingTransformRecipeBuilder;
+import tamaized.voidscape.datagen.data.recipe.IRecipeGenerator;
+import tamaized.voidscape.datagen.util.RecipeProviderUtil;
+import tamaized.voidscape.registry.ModItemComponents;
+import tamaized.voidscape.registry.tool.ModItemComponentDirectory;
+
+@Component
+public class VoidicAxeFangRecipeGenerator implements IRecipeGenerator {
+
+	@Autowired
+	private ModItemComponentDirectory items;
+
+	@Autowired
+	private ModItemComponents itemComponents;
+
+	@Autowired
+	private RecipeProviderUtil recipeProviderUtil;
+
+	@Override
+	public void generate(RecipeOutput recipeOutput) {
+		ItemStack result = new ItemStack(items.toolSetComponentDirectory().voidicCrystalToolSet().VOIDIC_CRYSTAL_AXE.get());
+		result.set(itemComponents.FANG, true);
+		ExtendedSmithingTransformRecipeBuilder.extendedSmithing(
+			Ingredient.of(items.augmentItems().VOIDIC_TEMPLATE.get()),
+			Ingredient.of(items.toolSetComponentDirectory().voidicCrystalToolSet().VOIDIC_CRYSTAL_AXE.get()),
+			Ingredient.of(items.augmentItems().ETHEREAL_SPIDER_FANG.get()),
+			RecipeCategory.COMBAT,
+			result
+		)
+			.unlocks("has_template", recipeProviderUtil.has(items.augmentItems().VOIDIC_TEMPLATE.get()))
+			.save(recipeOutput, "voidic_crystal_axe_fang");
+	}
+
+}
---
+++ src/data/java/tamaized/voidscape/datagen/data/recipe/voidic/VoidicLeggingsRecipeGenerator.java
@@ -0,0 +1,36 @@
+package tamaized.voidscape.datagen.data.recipe.voidic;
+
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.SmithingTransformRecipeBuilder;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import tamaized.beanification.Autowired;
+import tamaized.beanification.Component;
+import tamaized.voidscape.datagen.data.recipe.IRecipeGenerator;
+import tamaized.voidscape.datagen.util.RecipeProviderUtil;
+import tamaized.voidscape.registry.tool.ModItemComponentDirectory;
+
+@Component
+public class VoidicLeggingsRecipeGenerator implements IRecipeGenerator {
+
+	@Autowired
+	private ModItemComponentDirectory items;
+
+	@Autowired
+	private RecipeProviderUtil recipeProviderUtil;
+
+	@Override
+	public void generate(RecipeOutput recipeOutput) {
+		SmithingTransformRecipeBuilder.smithing(
+			Ingredient.of(items.augmentItems().VOIDIC_TEMPLATE.get()),
+			Ingredient.of(Items.NETHERITE_LEGGINGS),
+			Ingredient.of(items.materialItems().VOIDIC_CRYSTAL.get()),
+			RecipeCategory.COMBAT,
+			items.modArmorSetComponentDirectory().voidicCrystalArmorSet().VOIDIC_CRYSTAL_LEGS.get()
+		)
+			.unlocks("has_template", recipeProviderUtil.has(items.augmentItems().VOIDIC_TEMPLATE.get()))
+			.save(recipeOutput, "voidic_crystal_leggings");
+	}
+
+}
