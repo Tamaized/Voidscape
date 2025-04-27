bb01a12c162bd45b3c69426486da668751da4893
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-04-27 18:35:48 -0400

    Fix order of operations crash with Registries.ITEM

--- src/main/java/tamaized/voidscape/Voidscape.java
+++ src/main/java/tamaized/voidscape/Voidscape.java
@@ -1,5 +1,6 @@
 package tamaized.voidscape;
 
+import net.minecraft.core.registries.Registries;
 import net.neoforged.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,6 +15,7 @@ public class Voidscape {
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 
 	static {
+		RegUtil.create(Registries.ITEM); // Ensure this exists during bean init
 		BeanContext.init(MODID);
 		RegUtil.setup();
 	}
