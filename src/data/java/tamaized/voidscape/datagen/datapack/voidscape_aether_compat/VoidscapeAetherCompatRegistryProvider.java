1bf22b0d2f11681e0e4890aa1cc6978add37af12
kamuikeshin <211971195+kamuikeshin@users.noreply.github.com>
2025-07-04 16:27:54 -0400

    voidscape aether compat datapack pack.mcmeta datagen

--- src/data/java/tamaized/voidscape/datagen/datapack/voidscape_aether_compat/VoidscapeAetherCompatRegistryProvider.java
+++ src/data/java/tamaized/voidscape/datagen/datapack/voidscape_aether_compat/VoidscapeAetherCompatRegistryProvider.java
@@ -2,11 +2,16 @@ package tamaized.voidscape.datagen.datapack.voidscape_aether_compat;
 
 import com.aetherteam.aether.data.generators.AetherRegistrySets;
 import com.mojang.serialization.DynamicOps;
+import net.minecraft.DetectedVersion;
 import net.minecraft.core.*;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.metadata.PackMetadataGenerator;
 import net.minecraft.resources.RegistryOps;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
+import net.minecraft.util.InclusiveRange;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
@@ -55,59 +60,72 @@ public class VoidscapeAetherCompatRegistryProvider {
 			.sorted(Comparator.comparingInt(IBootstrap::priority))
 			.forEach(bootstrap -> builder = bootstrap.bootstrap(builder));
 
-		bus.addListener(GatherDataEvent.class, event -> event.getGenerator()
-			.getBuiltinDatapack(event.includeServer(), "minecraft", "voidscape_aether_compat")
-			.addProvider(f -> new DatapackBuiltinEntriesProvider(
-				new PackOutput(f.getOutputFolder()),
-				event.getLookupProvider().thenApply(v -> new HolderLookup.Provider() {
-					@Override
-					public Stream<ResourceKey<? extends Registry<?>>> listRegistries() {
-						return Stream.concat(v.listRegistries(), getAetherRegistries().join().patches().listRegistries())
-							.collect(Collectors.toSet())
-							.stream();
-					}
+		bus.addListener(GatherDataEvent.class, event -> {
+			event.getGenerator()
+				.getBuiltinDatapack(event.includeServer(), "minecraft", "voidscape_aether_compat")
+				.addProvider(f -> new DatapackBuiltinEntriesProvider(
+					new PackOutput(f.getOutputFolder()),
+					event.getLookupProvider().thenApply(v -> new HolderLookup.Provider() {
+						@Override
+						public Stream<ResourceKey<? extends Registry<?>>> listRegistries() {
+							return Stream.concat(v.listRegistries(), getAetherRegistries().join().patches().listRegistries())
+								.collect(Collectors.toSet())
+								.stream();
+						}
 
-					@Override
-					public <T> Optional<HolderLookup.RegistryLookup<T>> lookup(ResourceKey<? extends Registry<? extends T>> registryKey) {
-						if (registryKey.equals(Registries.BIOME))
-							return v.lookup(registryKey);
+						@Override
+						public <T> Optional<HolderLookup.RegistryLookup<T>> lookup(ResourceKey<? extends Registry<? extends T>> registryKey) {
+							if (registryKey.equals(Registries.BIOME))
+								return v.lookup(registryKey);
 
-						Optional<HolderLookup.RegistryLookup<T>> root = v.lookup(registryKey);
-						Optional<HolderLookup.RegistryLookup<T>> aether = VoidscapeAetherCompatRegistryProvider.this.lookup(registryKey);
-						HolderOwner<T> owner = new HolderOwner<>() {
-							@Override
-							public boolean canSerializeIn(HolderOwner<T> owner) {
-								return owner == this || root.map(o -> o.canSerializeIn(owner)).orElse(false) || aether.map(o -> o.canSerializeIn(owner)).orElse(false);
-							}
-						};
-						Map<ResourceKey<T>, Holder.Reference<T>> map = new HashMap<>();
-						Stream.of(root, aether).forEach(r -> r.ifPresent(l -> l.listElements().forEach(e -> {
-							Holder.Reference<T> ref = Holder.Reference.createStandAlone(owner, Objects.requireNonNull(e.getKey()));
-							holderReferenceBinderUtil.bindValue(ref, e.value());
-							map.put(e.key(), ref);
-						})));
-						return Optional.of(RegistrySetBuilder.lookupFromMap(
-							registryKey,
-							root.or(() -> aether).map(HolderLookup.RegistryLookup::registryLifecycle).orElseThrow(),
-							owner,
-							map
-						));
-					}
+							Optional<HolderLookup.RegistryLookup<T>> root = v.lookup(registryKey);
+							Optional<HolderLookup.RegistryLookup<T>> aether = VoidscapeAetherCompatRegistryProvider.this.lookup(registryKey);
+							HolderOwner<T> owner = new HolderOwner<>() {
+								@Override
+								public boolean canSerializeIn(HolderOwner<T> owner) {
+									return owner == this || root.map(o -> o.canSerializeIn(owner)).orElse(false) || aether.map(o -> o.canSerializeIn(owner)).orElse(false);
+								}
+							};
+							Map<ResourceKey<T>, Holder.Reference<T>> map = new HashMap<>();
+							Stream.of(root, aether).forEach(r -> r.ifPresent(l -> l.listElements().forEach(e -> {
+								Holder.Reference<T> ref = Holder.Reference.createStandAlone(owner, Objects.requireNonNull(e.getKey()));
+								holderReferenceBinderUtil.bindValue(ref, e.value());
+								map.put(e.key(), ref);
+							})));
+							return Optional.of(RegistrySetBuilder.lookupFromMap(
+								registryKey,
+								root.or(() -> aether).map(HolderLookup.RegistryLookup::registryLifecycle).orElseThrow(),
+								owner,
+								map
+							));
+						}
 
-					@Override
-					public <V> RegistryOps<V> createSerializationContext(DynamicOps<V> ops) {
-						RegistryOps<V> root = v.createSerializationContext(ops);
-						RegistryOps<V> aether = RegistryOps.create(ops, getAetherRegistries().join().patches());
-						return RegistryOps.create(ops, new RegistryOps.RegistryInfoLookup() {
-							public <T> Optional<RegistryOps.RegistryInfo<T>> lookup(ResourceKey<? extends Registry<? extends T>> key) {
-								return root.lookupProvider.lookup(key).or(() -> aether.lookupProvider.lookup(key));
-							}
-						});
-					}
-				}),
-				builder,
-				Set.of(Voidscape.MODID)
-			)));
+						@Override
+						public <V> RegistryOps<V> createSerializationContext(DynamicOps<V> ops) {
+							RegistryOps<V> root = v.createSerializationContext(ops);
+							RegistryOps<V> aether = RegistryOps.create(ops, getAetherRegistries().join().patches());
+							return RegistryOps.create(ops, new RegistryOps.RegistryInfoLookup() {
+								public <T> Optional<RegistryOps.RegistryInfo<T>> lookup(ResourceKey<? extends Registry<? extends T>> key) {
+									return root.lookupProvider.lookup(key).or(() -> aether.lookupProvider.lookup(key));
+								}
+							});
+						}
+					}),
+					builder,
+					Set.of(Voidscape.MODID)
+				));
+
+			event.getGenerator()
+				.getBuiltinDatapack(event.includeServer(), "minecraft", "voidscape_aether_compat")
+				.addProvider(f -> new PackMetadataGenerator(new PackOutput(f.getOutputFolder()))
+					.add(PackMetadataSection.TYPE, new PackMetadataSection(
+							net.minecraft.network.chat.Component.literal("Resources for Voidscape - Aether Compat"),
+							DetectedVersion.BUILT_IN.getPackVersion(PackType.SERVER_DATA),
+							Optional.of(new InclusiveRange<>(0, Integer.MAX_VALUE))
+						)
+					)
+				);
+		});
 	}
 
 	private CompletableFuture<RegistrySetBuilder.PatchedRegistries> getAetherRegistries() {
@@ -126,7 +144,7 @@ public class VoidscapeAetherCompatRegistryProvider {
 	}
 
 	public <T> Holder.Reference<T> createOwnedHolderReference(ResourceKey<Registry<T>> registry, ResourceKey<T> key) {
-		Holder.Reference<T> ref =  Holder.Reference.createStandAlone(getHolderOwner(), key);
+		Holder.Reference<T> ref = Holder.Reference.createStandAlone(getHolderOwner(), key);
 		holderReferenceBinderUtil.bindValue(ref, lookup(registry).orElseThrow().get(key).orElseThrow().value());
 		return ref;
 	}
