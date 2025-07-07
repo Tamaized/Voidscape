4e834ae0da1f6c963b3824fed025d065f52fb0a8
kamuikeshin <211971195+kamuikeshin@users.noreply.github.com>
2025-07-07 10:16:42 -0400

    Fix Spell Tomes

--- src/main/java/tamaized/voidscape/item/SpellTomeItem.java
+++ src/main/java/tamaized/voidscape/item/SpellTomeItem.java
@@ -33,10 +33,12 @@ public class SpellTomeItem extends Item {
 		return repairStack.is(repairMaterial.get()) || super.isValidRepairItem(stack, repairStack);
 	}
 
-	public int getUseDuration(ItemStack stack) {
+	@Override
+	public int getUseDuration(ItemStack stack, LivingEntity entity) {
 		return 72000;
 	}
 
+	@Override
 	public UseAnim getUseAnimation(ItemStack stack) {
 		return UseAnim.BOW;
 	}
@@ -44,12 +46,12 @@ public class SpellTomeItem extends Item {
 	@Override
 	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
 		player.startUsingItem(hand);
-		return InteractionResultHolder.success(player.getItemInHand(hand));
+		return InteractionResultHolder.consume(player.getItemInHand(hand));
 	}
 
 	@Override
 	public void releaseUsing(ItemStack stack, Level level, LivingEntity entity, int timeLeft) {
-		if (getUseDuration(stack) - timeLeft > 15) {
+		if (getUseDuration(stack, entity) - timeLeft > 15) {
 			action.accept(new ActionContext(stack, level, entity));
 			level.playSound(null, entity.position().x(), entity.position().y(), entity.position().z(), SoundEvents.BLAZE_SHOOT, SoundSource.PLAYERS, 1F, 0.5F + entity.getRandom().nextFloat() * 0.25F);
 			if (entity instanceof Player player)
