683cd570f051a58d1091737490194e4c47ca82b9
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-05-31 11:25:00 -0400

    Clean up tool tags

--- src/data/java/tamaized/voidscape/datagen/data/tag/item/BowItemTagProviderFactory.java
+++ src/data/java/tamaized/voidscape/datagen/data/tag/item/BowItemTagProviderFactory.java
@@ -2,8 +2,10 @@ package tamaized.voidscape.datagen.data.tag.item;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.tags.ItemTags;
+import net.neoforged.neoforge.common.Tags;
 import tamaized.beanification.Autowired;
 import tamaized.beanification.Component;
+import tamaized.voidscape.datagen.util.TagProviderUtil;
 import tamaized.voidscape.registry.ModToolSetComponentDirectory;
 
 @Component
@@ -12,14 +14,24 @@ public class BowItemTagProviderFactory implements IItemTagProviderFactory {
 	@Autowired
 	private ModToolSetComponentDirectory tools;
 
+	@Autowired
+	private TagProviderUtil tagProviderUtil;
+
 	@Override
 	public void make(ItemTagProviderFactory.ItemTagsProviderAccessor accessor, HolderLookup.Provider provider) {
-		accessor.tag(ItemTags.BOW_ENCHANTABLE).add(
-			tools.voidicCrystalToolSet().VOIDIC_CRYSTAL_BOW.get(),
-			tools.corruptToolSet().CORRUPT_BOW.get(),
-			tools.titaniteToolSet().TITANITE_BOW.get(),
-			tools.ichorToolSet().ICHOR_BOW.get(),
-			tools.astralToolSet().ASTRAL_BOW.get()
+		tagProviderUtil.multiTagAll(
+			accessor::tag,
+			Tags.Items.TOOLS_BOW,
+			Tags.Items.RANGED_WEAPON_TOOLS,
+			ItemTags.BOW_ENCHANTABLE,
+			ItemTags.DURABILITY_ENCHANTABLE,
+			ItemTags.VANISHING_ENCHANTABLE
+		).add(
+			tools.voidicCrystalToolSet().VOIDIC_CRYSTAL_BOW.getKey(),
+			tools.corruptToolSet().CORRUPT_BOW.getKey(),
+			tools.titaniteToolSet().TITANITE_BOW.getKey(),
+			tools.ichorToolSet().ICHOR_BOW.getKey(),
+			tools.astralToolSet().ASTRAL_BOW.getKey()
 		);
 	}
 }
