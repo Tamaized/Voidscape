7ac1700c8945fe2a3fa1c507036a55d300e1d071
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-05-31 10:23:09 -0400

    I hate this but it actually works

--- src/asm/java/tamaized/voidscape/coremod/transformers/package-info.java
+++ src/asm/java/tamaized/voidscape/coremod/transformers/package-info.java
@@ -2,7 +2,7 @@
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
 @TypeUseAreNonnullByDefault
-package tamaized.voidscape.coremod;
+package tamaized.voidscape.coremod.transformers;
 
 import com.mojang.logging.annotations.FieldsAreNonnullByDefault;
 import com.mojang.logging.annotations.MethodsReturnNonnullByDefault;
