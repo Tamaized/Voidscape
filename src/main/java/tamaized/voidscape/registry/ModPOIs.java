caff2384159638120cd36de0dbc24bfb282770bf
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-06-19 11:07:43 -0400

    Fix ModPOIs

--- src/main/java/tamaized/voidscape/registry/ModPOIs.java
+++ src/main/java/tamaized/voidscape/registry/ModPOIs.java
@@ -4,7 +4,6 @@ import com.google.common.collect.ImmutableSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
 import tamaized.beanification.Autowired;
 import tamaized.beanification.Component;
 import tamaized.regutil.RegUtil;
@@ -16,8 +15,7 @@ public class ModPOIs {
 	@Autowired
 	private FunctionalBlocks functionalBlocks;
 
-	private final DeferredRegister<PoiType> REGISTERY = RegUtil.create(Registries.POINT_OF_INTEREST_TYPE);
-
-	public final DeferredHolder<PoiType, PoiType> PORTAL = REGISTERY.register("portal", () -> new PoiType(ImmutableSet.copyOf(functionalBlocks.PORTAL.get().getStateDefinition().getPossibleStates()), 0, 1));
+	public final DeferredHolder<PoiType, PoiType> PORTAL = RegUtil.register(Registries.POINT_OF_INTEREST_TYPE, "portal",
+		() -> new PoiType(ImmutableSet.copyOf(functionalBlocks.PORTAL.get().getStateDefinition().getPossibleStates()), 0, 1));
 
 }
