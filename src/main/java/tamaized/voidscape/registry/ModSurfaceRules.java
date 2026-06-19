759e4cddf6682014126f13d6cb57bdbe2823851c
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-06-19 11:06:06 -0400

    Fix ModSurfaceRules

--- src/main/java/tamaized/voidscape/registry/ModSurfaceRules.java
+++ src/main/java/tamaized/voidscape/registry/ModSurfaceRules.java
@@ -2,8 +2,6 @@ package tamaized.voidscape.registry;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.world.level.levelgen.SurfaceRules;
-import net.neoforged.neoforge.registries.DeferredRegister;
 import tamaized.beanification.Component;
 import tamaized.regutil.RegUtil;
 import tamaized.voidscape.surfacerule.AirAboveConditionSource;
@@ -13,8 +11,6 @@ import java.util.function.Supplier;
 @Component
 public class ModSurfaceRules {
 
-	private final DeferredRegister<MapCodec<? extends SurfaceRules.ConditionSource>> REGISTRY = RegUtil.create(Registries.MATERIAL_CONDITION);
-
-	public final Supplier<MapCodec<AirAboveConditionSource>> AIR_ABOVE = REGISTRY.register("air_above", AirAboveConditionSource.CODEC::codec);
+	public final Supplier<MapCodec<AirAboveConditionSource>> AIR_ABOVE = RegUtil.register(Registries.MATERIAL_CONDITION, "air_above", AirAboveConditionSource.CODEC::codec);
 
 }
