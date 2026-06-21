30cb225db86c83c40ace1cf93153828205dd270f
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-06-21 12:32:53 -0400

    Fix ModAdvancementTriggers

--- src/main/java/tamaized/voidscape/registry/ModAdvancementTriggers.java
+++ src/main/java/tamaized/voidscape/registry/ModAdvancementTriggers.java
@@ -1,8 +1,6 @@
 package tamaized.voidscape.registry;
 
-import net.minecraft.advancements.CriterionTrigger;
 import net.minecraft.core.registries.Registries;
-import net.neoforged.neoforge.registries.DeferredRegister;
 import tamaized.beanification.Component;
 import tamaized.regutil.RegUtil;
 import tamaized.voidscape.advancement.*;
@@ -12,22 +10,20 @@ import java.util.function.Supplier;
 @Component
 public class ModAdvancementTriggers {
 
-	private final DeferredRegister<CriterionTrigger<?>> REGISTRY = RegUtil.create(Registries.TRIGGER_TYPE);
-
-	public final Supplier<GenericAdvancementTrigger> ETHEREAL_ESSENCE_TRIGGER = REGISTRY.register("ethereal_essence", GenericAdvancementTrigger::new);
-	public final Supplier<GenericAdvancementTrigger> ETHEREAL_SPIDER_EGGS_TRIGGER = REGISTRY.register("ethereal_spider_eggs", GenericAdvancementTrigger::new);
-	public final Supplier<ItemMatchesAdvancementTrigger> ITEM_USED_ON_NULL_SERVANT_TRIGGER = REGISTRY.register("item_used_on_null_servant", ItemMatchesAdvancementTrigger::new);
-	public final Supplier<GenericAdvancementTrigger> ACTIVATE_PORTAL_TRIGGER = REGISTRY.register("activate_portal", GenericAdvancementTrigger::new);
-	public final Supplier<GenericAdvancementTrigger> INFUSED_TRIGGER = REGISTRY.register("infused", GenericAdvancementTrigger::new);
-	public final Supplier<GenericAdvancementTrigger> HOE_BONEMEAL_TRIGGER = REGISTRY.register("hoe_bonemeal", GenericAdvancementTrigger::new);
-	public final Supplier<GenericAdvancementTrigger> LIQUIFIER_TRIGGER = REGISTRY.register("liquifier", GenericAdvancementTrigger::new);
-	public final Supplier<GenericAdvancementTrigger> DEFUSER_TRIGGER = REGISTRY.register("defuser", GenericAdvancementTrigger::new);
-	public final Supplier<GenericAdvancementTrigger> GERMINATOR_TRIGGER = REGISTRY.register("germinator", GenericAdvancementTrigger::new);
-	public final Supplier<GenericAdvancementTrigger> WELL_TRIGGER = REGISTRY.register("well", GenericAdvancementTrigger::new);
-	public final Supplier<GenericAdvancementTrigger> COOP_TRIGGER = REGISTRY.register("coop", GenericAdvancementTrigger::new);
-	public final Supplier<GenericAdvancementTrigger> HATCHERY_TRIGGER = REGISTRY.register("hatchery", GenericAdvancementTrigger::new);
-	public final Supplier<GenericAdvancementTrigger> INFUSER_TRIGGER = REGISTRY.register("infuser", GenericAdvancementTrigger::new);
-	public final Supplier<GenericAdvancementTrigger> COLLECTOR_TRIGGER = REGISTRY.register("collector", GenericAdvancementTrigger::new);
-	public final Supplier<ItemMatchesAdvancementTrigger> THREE_BY_THREE = REGISTRY.register("three_by_three", ItemMatchesAdvancementTrigger::new);
+	public final Supplier<GenericAdvancementTrigger> ETHEREAL_ESSENCE_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "ethereal_essence", GenericAdvancementTrigger::new);
+	public final Supplier<GenericAdvancementTrigger> ETHEREAL_SPIDER_EGGS_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "ethereal_spider_eggs", GenericAdvancementTrigger::new);
+	public final Supplier<ItemMatchesAdvancementTrigger> ITEM_USED_ON_NULL_SERVANT_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "item_used_on_null_servant", ItemMatchesAdvancementTrigger::new);
+	public final Supplier<GenericAdvancementTrigger> ACTIVATE_PORTAL_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "activate_portal", GenericAdvancementTrigger::new);
+	public final Supplier<GenericAdvancementTrigger> INFUSED_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "infused", GenericAdvancementTrigger::new);
+	public final Supplier<GenericAdvancementTrigger> HOE_BONEMEAL_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "hoe_bonemeal", GenericAdvancementTrigger::new);
+	public final Supplier<GenericAdvancementTrigger> LIQUIFIER_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "liquifier", GenericAdvancementTrigger::new);
+	public final Supplier<GenericAdvancementTrigger> DEFUSER_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "defuser", GenericAdvancementTrigger::new);
+	public final Supplier<GenericAdvancementTrigger> GERMINATOR_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "germinator", GenericAdvancementTrigger::new);
+	public final Supplier<GenericAdvancementTrigger> WELL_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "well", GenericAdvancementTrigger::new);
+	public final Supplier<GenericAdvancementTrigger> COOP_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "coop", GenericAdvancementTrigger::new);
+	public final Supplier<GenericAdvancementTrigger> HATCHERY_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "hatchery", GenericAdvancementTrigger::new);
+	public final Supplier<GenericAdvancementTrigger> INFUSER_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "infuser", GenericAdvancementTrigger::new);
+	public final Supplier<GenericAdvancementTrigger> COLLECTOR_TRIGGER = RegUtil.register(Registries.TRIGGER_TYPE, "collector", GenericAdvancementTrigger::new);
+	public final Supplier<ItemMatchesAdvancementTrigger> THREE_BY_THREE = RegUtil.register(Registries.TRIGGER_TYPE, "three_by_three", ItemMatchesAdvancementTrigger::new);
 
 }
