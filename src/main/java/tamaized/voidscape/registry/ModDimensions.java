f3e6c92840d59bcf146fab010f2679b1a3d38f43
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-06-21 11:38:04 -0400

    Fix ModDimensions

--- src/main/java/tamaized/voidscape/registry/ModDimensions.java
+++ src/main/java/tamaized/voidscape/registry/ModDimensions.java
@@ -4,29 +4,12 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.level.Level;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.client.event.RegisterDimensionSpecialEffectsEvent;
-import tamaized.beanification.Autowired;
 import tamaized.beanification.Component;
-import tamaized.beanification.PostConstruct;
 import tamaized.voidscape.Voidscape;
-import tamaized.voidscape.client.VoidDimensionSpecialEffectsFactory;
 
 @Component
 public class ModDimensions {
 
-	@Autowired(dist = Dist.CLIENT)
-	private VoidDimensionSpecialEffectsFactory voidDimensionSpecialEffectsFactory;
-
 	public final ResourceKey<Level> VOID = ResourceKey.create(Registries.DIMENSION, Identifier.fromNamespaceAndPath(Voidscape.MODID, "void"));
-
-	@PostConstruct
-	private void setup(IEventBus bus) {
-		bus.addListener(RegisterDimensionSpecialEffectsEvent.class, event -> event.register(
-			VOID.location(),
-			voidDimensionSpecialEffectsFactory.make()
-		));
-	}
 
 }
