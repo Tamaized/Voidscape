d847336933ea491685baf5af01ef0a58c79f7a91
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-08-22 14:04:54 -0400

    Port EntityType tag datagen

--- src/data/java/tamaized/voidscape/datagen/data/tag/entity/IEntityTypeTagProviderFactory.java
+++ src/data/java/tamaized/voidscape/datagen/data/tag/entity/IEntityTypeTagProviderFactory.java
@@ -1,9 +1,10 @@
 package tamaized.voidscape.datagen.data.tag.entity;
 
 import net.minecraft.core.HolderLookup;
+import tamaized.datagenutil.data.tag.ExposedEntityTypeTagProvider;
 
 public interface IEntityTypeTagProviderFactory {
 
-	void make(EntityTypeTagProviderFactory.EntityTypeTagsProviderAccessor accessor, HolderLookup.Provider provider);
+	void make(ExposedEntityTypeTagProvider accessor, HolderLookup.Provider provider);
 
 }
--- src/data/java/tamaized/voidscape/datagen/data/tag/entity/RedirectableProjectileEntityTypeTagProviderFactory.java
+++ src/data/java/tamaized/voidscape/datagen/data/tag/entity/RedirectableProjectileEntityTypeTagProviderFactory.java
@@ -4,6 +4,7 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.tags.EntityTypeTags;
 import tamaized.beanification.Autowired;
 import tamaized.beanification.Component;
+import tamaized.datagenutil.data.tag.ExposedEntityTypeTagProvider;
 import tamaized.voidscape.registry.ModEntities;
 
 @Component
@@ -13,7 +14,7 @@ public class RedirectableProjectileEntityTypeTagProviderFactory implements IEnti
 	private ModEntities entities;
 
 	@Override
-	public void make(EntityTypeTagProviderFactory.EntityTypeTagsProviderAccessor accessor, HolderLookup.Provider provider) {
+	public void make(ExposedEntityTypeTagProvider accessor, HolderLookup.Provider provider) {
 		accessor.tag(EntityTypeTags.REDIRECTABLE_PROJECTILE).add(
 			entities.STRANGE_PEARL.get()
 		);
