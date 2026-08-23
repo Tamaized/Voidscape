ec21c1a64821c25ac823d2a5bde71e74942aec78
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-08-23 18:00:17 -0400

    Port DimensionTypeBootstrap

--- src/data/java/tamaized/voidscape/datagen/bootstrap/DimensionTypeBootstrap.java
+++ src/data/java/tamaized/voidscape/datagen/bootstrap/DimensionTypeBootstrap.java
@@ -1,18 +1,25 @@
 package tamaized.voidscape.datagen.bootstrap;
 
 import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TimelineTags;
 import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.world.attribute.BedRule;
+import net.minecraft.world.attribute.EnvironmentAttributeMap;
+import net.minecraft.world.attribute.EnvironmentAttributes;
+import net.minecraft.world.level.CardinalLighting;
 import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.timeline.Timeline;
+import org.jetbrains.annotations.Nullable;
 import tamaized.beanification.Autowired;
 import tamaized.beanification.Component;
 import tamaized.voidscape.registry.ModDimensions;
 
-import javax.annotation.Nullable;
-import java.util.OptionalLong;
+import java.util.Optional;
 
 @Component
 public class DimensionTypeBootstrap implements IBootstrap {
@@ -26,24 +33,29 @@ public class DimensionTypeBootstrap implements IBootstrap {
 	@Override
 	public RegistrySetBuilder bootstrap(RegistrySetBuilder builder) {
 		return builder.add(Registries.DIMENSION_TYPE, context -> {
+			HolderGetter<Timeline> timelines = context.lookup(Registries.TIMELINE);
 			VOID = context.register(
 				ResourceKey.create(Registries.DIMENSION_TYPE, dimensions.VOID.identifier()),
 				new DimensionType(
-					OptionalLong.of(6000L),
-					false,
+					true,
 					false,
 					false,
 					false,
 					1.0D,
-					false,
-					false,
 					0,
 					256,
 					256,
 					BlockTags.INFINIBURN_OVERWORLD,
-					dimensions.VOID.identifier(),
 					-0.3F,
-					new DimensionType.MonsterSettings(ConstantInt.ZERO, 0)
+					new DimensionType.MonsterSettings(ConstantInt.ZERO, 0),
+					DimensionType.Skybox.NONE,
+					CardinalLighting.Type.DEFAULT,
+					EnvironmentAttributeMap.builder()
+						.set(EnvironmentAttributes.CAN_START_RAID, false)
+						.set(EnvironmentAttributes.BED_RULE, BedRule.EXPLODES)
+						.build(),
+					timelines.getOrThrow(TimelineTags.UNIVERSAL),
+					Optional.empty()
 				)
 			);
 		});
