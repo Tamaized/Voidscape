ae0f8a307a10a6df851144065f5509f0b5cbe174
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-08-27 19:18:18 -0400

    Fix axes and the charred warhammer missing the sweeping edge tag

--- src/data/java/tamaized/voidscape/datagen/data/tag/item/EnchantableSwordLikeItemTagProviderFactory.java
+++ src/data/java/tamaized/voidscape/datagen/data/tag/item/EnchantableSwordLikeItemTagProviderFactory.java
@@ -6,25 +6,30 @@ import net.minecraft.world.item.Item;
 import tamaized.beanification.Autowired;
 import tamaized.beanification.Component;
 import tamaized.datagenutil.data.tag.ExposedKeyTagProvider;
+import tamaized.datagenutil.data.tag.TagProviderUtil;
 import tamaized.voidscape.registry.ModToolSetComponentDirectory;
 
 import java.util.List;
 
 @Component
-public class EnchantableSwordItemTagProviderFactory implements IItemTagProviderFactory {
+public class EnchantableSwordLikeItemTagProviderFactory implements IItemTagProviderFactory {
+
+	@Autowired
+	private TagProviderUtil tagProviderUtil;
 
 	@Autowired
 	private ModToolSetComponentDirectory tools;
 
 	@Override
 	public void make(ExposedKeyTagProvider<Item> accessor, HolderLookup.Provider provider) {
-		accessor.tag(ItemTags.MELEE_WEAPON_ENCHANTABLE).addAll(List.of(
+		tagProviderUtil.tagMany(accessor::tag, appender -> appender.addAll(List.of(
 			tools.voidicCrystalToolSet().VOIDIC_CRYSTAL_AXE.getKey(),
 			tools.charredToolSet().CHARRED_WARHAMMER.getKey(),
 			tools.corruptToolSet().CORRUPT_AXE.getKey(),
 			tools.titaniteToolSet().TITANITE_AXE.getKey(),
 			tools.ichorToolSet().ICHOR_AXE.getKey(),
 			tools.astralToolSet().ASTRAL_AXE.getKey()
-		));
+		)), ItemTags.MELEE_WEAPON_ENCHANTABLE, ItemTags.SWEEPING_ENCHANTABLE);
 	}
+
 }
